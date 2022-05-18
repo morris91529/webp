305c0301c395c921a5310bec9277fb487bb8abc5
morris91529 <102048275+morris91529@users.noreply.github.com>
2022-05-18 23:07:39 +0800

    Add files via upload

---
+++ Hw8/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SignIn from './signin';
+
+function App() {
+  return (
+    <div className="App">
+    {SignIn()}
+    </div>
+  );
+}
+
+export default App;
---
+++ Hw8/src/Component/button.js
@@ -0,0 +1,9 @@
+import Button from '@mui/material/Button';
+
+const button = () => {
+    var output=[];
+    output.push(<div><Button variant="contained" color="primary">sign in</Button></div>)
+    return output;
+}
+
+export default button;
